04150ab29c88e33c3d2214aaa13db13350c4b083
EARNascimento <eduardo90909@outlook.com>
2025-08-07 17:12:50 -0300

    Adicionando as funções de busca por trecho do nome d episódio, busca dos melhores episódios de uma série e a busca de episódios por ano

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -29,6 +29,8 @@ public class Principal {
         this.repositorio = repositorio;
     }
 
+    private Optional<Serie> serieBusca;
+
     public void exibeMenu(){
         var opcao = -1;
         while(opcao != 0 ) {
@@ -41,6 +43,9 @@ public class Principal {
                     6 - Top 5 séries
                     7 - Buscar série por categoria
                     8 - Buscar série por temporada e avaliação
+                    9 - Buscar episódio por trecho
+                    10 - Top 5 episódios por série
+                    11 - Buscar episódios a partir de uma data
                     
                     0 - Sair
                     """;
@@ -74,6 +79,14 @@ public class Principal {
                 case 8:
                     filtrarSeriePorTemporadaEAvaliacao();
                     break;
+                case 9:
+                    buscarEpisodioPorTrecho();
+                    break;
+                case 10:
+                    topEpisodioPorSerie();
+                    break;
+                case 11:
+                    buscarEpisodioDepoisDeUmaData();
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -140,10 +153,10 @@ public class Principal {
     private void buscarSeriePorTitulo(){
         System.out.println("Escolha uma série por nome: ");
         var nomeSerie = leitura.nextLine();
-        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+        serieBusca = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
-        if(serieBuscada.isPresent()){
-            System.out.println("Dados da série: " + serieBuscada.get());
+        if(serieBusca.isPresent()){
+            System.out.println("Dados da série: " + serieBusca.get());
         } else {
             System.out.println("Série não encontrada.");
         }
@@ -186,6 +199,43 @@ public class Principal {
         System.out.println("*** Séries filtrada ***");
         filtroSeries.forEach(s ->
                 System.out.println(s.getTitulo() + " - avaliação: " + s.getAvaliacao() ));
+    }
+
+    private void buscarEpisodioPorTrecho(){
+        System.out.println("Qual o nome do episódio para busca? ");
+        var trechoEpisodio = leitura.nextLine();
+        List<Episodio> episodiosEncontrados = repositorio.episodiosPorTrecho(trechoEpisodio);
+        episodiosEncontrados.forEach(e ->
+                System.out.printf("Série: %s Temporada %s - Episódio %s - %s \n",
+                        e.getSerie().getTitulo(), e.getTemporada(),
+                        e.getNumeroEpisodio(), e.getTitulo()));
+
+    }
+
+    private void topEpisodioPorSerie(){
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            List<Episodio> topEpisodios = repositorio.topEpisodiosPorSerie(serie);
+            topEpisodios.forEach(e ->
+                    System.out.printf("Série: %s Temporada %s - Episódio %s - %s Avaliação %s\n ",
+                            e.getSerie().getTitulo(), e.getTemporada(),
+                            e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        }
+    }
+
+    private void buscarEpisodioDepoisDeUmaData(){
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lançamento: ");
+            var anoLancamento = leitura.nextInt();
+            leitura.nextLine();
+
+            List<Episodio> episodiosAno = repositorio.episodiosPorSerieEAno(serie, anoLancamento);
+
+            episodiosAno.forEach(System.out::println);
+        }
     }
 //    public void exibeMenu(){
 //        System.out.println("Digite o nome da série: ");
