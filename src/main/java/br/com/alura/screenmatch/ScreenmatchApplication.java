af04c159acaf9c549e21943d273294b8ddd0cb3d
EARNascimento <eduardo90909@outlook.com>
2025-08-08 16:46:41 -0300

    Adicionando dependências para SpringWeb, adicionando dependências para resolver o problema de CORS, criando packages e classes para tratar erros e fazer uma conexão com o front-end

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -12,19 +12,11 @@ import java.io.IOException;
 import java.util.Properties;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 
-	@Autowired
-	private SerieRepository repositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
-	}
 }
